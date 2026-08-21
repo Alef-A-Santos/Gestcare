ffd6ff5f2972ffbb5294bf9c5b3943c583fd68dd
Guilherme Almeida FIEC <guilherme46193@fiec.edu.br>
2026-08-21 17:39:55 -0300

    ajuste de props da img

--- src/frontend/src/components/Logo.jsx
+++ src/frontend/src/components/Logo.jsx
@@ -1,7 +1,7 @@
 function Logo({ img, alt }) {
     return (
         <div>
-            <img className="w-18 h-18" src={img} alt={alt} />
+            <img className="w-30 h-28" src={img} alt={alt} />
         </div>
 
     )
--- src/frontend/src/pages/Home.jsx
+++ src/frontend/src/pages/Home.jsx
@@ -4,62 +4,59 @@ import Botao from "../components/Botao";
 
 import fundo from "../assets/imagem/fundo2.png";
 import logoRosa from "../assets/logos/logo_rosa.png";
-import fundoRosa from "../assets/imagem/FundoRosa.png"; 
-  function Home(){
-
- 
+import fundoRosa from "../assets/imagem/FundoRosa.png";
+function Home() {
   return (
     <div>
-       <div className="min-h-screen flex flex-col md:flex-row">
-      <div className="m-2 flex w-17 md:w-35 h-1/2 absolute">
-        <Logo imgurl={logoRosa} />
-      </div>
-
-      {/* LADO ESQUERDO */}
-
-      <div
-        className="w-full md:w-AUTO h-screen p-8 text-white  bg-cover bg-right hidden lg:flex"
-        style={{ backgroundImage: `url("${fundo}")` }}
-      >
-       
-      </div>
-
-      {/* LADO DIREITO */}
-
-      <main
-        className="w-dvw  lg:w-1/2 min-h-dvh flex items-center justify-center bg-[#f7cccc] p-4 object-cover"
-        style={{ backgroundImage: `url("${fundoRosa}")`, backgroundSize:"cover" }}
-      >
-        <div className="w-full h-full flex flex-col justify-center gap-5">
-          <h2 className="font-body text-center text-3xl  text-teal-500 mt-12">
-            <b>Entrar na sua conta:</b>
-
-            <p className="font-body text-sm text-gray-500 font-bold">
-              Acompanhe sua gravidez com segurança
-            </p>
-          </h2>
-
-          <InputHome />
-
-          <Botao
-            className="text-center bg-teal-500 hover:bg-teal-600 font-bold text-white rounded-lg py-3 m-2 w-full cursor-pointer"
-            nome={"Entrar"}
-          />
-
-          <div className="text-center font-mono">
-            <p>
-              Ainda não tem conta?{" "}
-              <a className="underline text-teal-500 font-bold" href="#">
-                Cadastre-se
-              </a>
-            </p>
-          </div>
+      <div className="min-h-screen flex flex-col md:flex-row">
+        <div className="m-2 flex w-20 md:w-35 h-1/2 absolute">
+          <Logo img={logoRosa} />
         </div>
-      </main>
+
+        {/* LADO ESQUERDO */}
+
+        <div
+          className="w-full md:w-AUTO h-screen p-8 text-white  bg-cover bg-right hidden lg:flex"
+          style={{ backgroundImage: `url("${fundo}")` }}
+        ></div>
+
+        {/* LADO DIREITO */}
+
+        <main
+          className="w-dvw  lg:w-1/2 min-h-dvh flex items-center justify-center bg-[#f7cccc] p-4 object-cover"
+          style={{
+            backgroundImage: `url("${fundoRosa}")`,
+            backgroundSize: "cover",
+          }}
+        >
+          <div className="w-full h-full flex flex-col justify-center gap-5">
+            <h2 className="font-body text-center text-3xl  text-teal-500 mt-12">
+              <b>Entrar na sua conta:</b>
+
+              <p className="font-body text-sm text-gray-500 font-bold">
+                Acompanhe sua gravidez com segurança
+              </p>
+            </h2>
+
+            <InputHome />
+
+            <Botao
+              className="text-center bg-teal-500 hover:bg-teal-600 font-bold text-white rounded-lg py-3 m-2 w-full cursor-pointer"
+              nome={"Entrar"}
+            />
+
+            <div className="text-center font-mono">
+              <p>
+                Ainda não tem conta?{" "}
+                <a className="underline text-teal-500 font-bold" href="#">
+                  Cadastre-se
+                </a>
+              </p>
+            </div>
+          </div>
+        </main>
+      </div>
     </div>
-  
-     
-    </div>
-  )
+  );
 }
-export default Home 
+export default Home;
