55d34cfc0d28ec56db295ab21e1aa928590a67e8
Guilherme Almeida FIEC <guilherme46193@fiec.edu.br>
2026-09-25 16:09:38 -0300

    resolvendo conflitos

--- src/frontend/src/components/Inputs.jsx
+++ src/frontend/src/components/Inputs.jsx
@@ -1,12 +1,12 @@
-function Inputs({icone,placeName,tipoDado,className,icone2,onInput,value,onChange,}) {
-  return (
-    <div className="relative flex flex-col gap-1">
-      {icone}
-      {icone2}
-      <input className={className} type={tipoDado} placeholder={placeName} value={value} onChange={onChange} onInput={onInput}
-      />
-    </div>
-  );
+function Inputs({icone, placeName, tipoDado, className, icone2}){
+     
+ 
+    return(
+        <div className="relative flex flex-col gap-1">
+            {icone}{icone2}
+            <input className={className} type={tipoDado} placeholder={placeName} required  />
+        </div>
+    )
 }
 
 export default Inputs;
